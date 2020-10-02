d39767740728f855d2d1491494e4cbdaf601c1af
Iqra-mcs <72266315+Iqra-mcs@users.noreply.github.com>
2020-10-02 20:44:13 +0500

    Create constants.js

---
+++ TP/constants.js
@@ -0,0 +1,6 @@
+var { _hash } = require("./lib");
+const cbor = require('cbor')
+const TP_FAMILY = 'simplestore';
+
+exports.TP_FAMILY = TP_FAMILY;
+exports.TP_NAMESPACE = _hash(TP_FAMILY).substring(0, 6);
